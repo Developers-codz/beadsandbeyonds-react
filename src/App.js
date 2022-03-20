e17b794c86d45c2f424211a07d78c1dd7111fd5f
developers-codz <kaj123al2002@gmail.com>
2022-03-20 19:27:16 +0530

    fix:removed unused lines

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Product />}>
-            <Route path=":categoryBy" element={<Product />} />
-          </Route>
+          <Route path="/products" element={<Product />} />
+
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/mockman" element={<Mockman />} />
