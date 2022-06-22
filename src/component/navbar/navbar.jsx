bc893d8a121a01ab2f03d811463571a474a6e2f3
developers-codz <kaj123al2002@gmail.com>
2022-06-22 13:25:01 +0530

    fix:signup icon

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,35 +1,35 @@
 import { useWishlist } from "context/wishlist-context";
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react";
 import { useCart } from "context/cart-context";
-import { NavLink, Link,useLocation,useNavigate } from "react-router-dom";
+import { NavLink, Link, useLocation, useNavigate } from "react-router-dom";
 import { useAside } from "context/aside-context";
-import {useProduct} from "context/product-context"
+import { useProduct } from "context/product-context";
 import "./navbar.css";
-import {debounce} from "functions"
+import { debounce } from "functions";
+import { useAuth } from "context/auth-context";
 const Navbar = () => {
-  const [searchBar,setSearchBar] = useState(false)
-  const [searchText,setSearchText] = useState("")
+  const [searchBar, setSearchBar] = useState(false);
+  const [searchText, setSearchText] = useState("");
   const { setActiveAside } = useAside();
   const { wishlistState } = useWishlist();
-  const {cartState  } = useCart();
-  const {dispatch} = useProduct();
+  const { cartState } = useCart();
+  const { dispatch } = useProduct();
   const location = useLocation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const path = location.pathname;
 
- 
-  const changeHandler = () =>{
-      dispatch({type:"SEARCH",payload: searchText}) 
-  }
+  const changeHandler = () => {
+    dispatch({ type: "SEARCH", payload: searchText });
+  };
 
-  const getOptimisedVersion = debounce(changeHandler,1000);
+  const getOptimisedVersion = debounce(changeHandler, 1000);
 
-  useEffect(()=>{
-    getOptimisedVersion()
-  },[searchText])
+  useEffect(() => {
+    getOptimisedVersion();
+  }, [searchText]);
   return (
     <>
-      <header className={path !== "/" ? "bottom-shadow":""} >
+      <header className={path !== "/" ? "bottom-shadow" : ""}>
         <div
           className="hamburger-wrapper"
           id="hamburger-btn"
@@ -64,18 +64,30 @@ const Navbar = () => {
             to="/profile/me"
             className="avatar avatar-badge text-primary reset"
           >
-            <i className="fa fa-user fa-lg" id="user-icon"></i>
+            {localStorage.getItem("token") ? (
+              <i className="fa fa-user fa-lg" id="user-icon"></i>
+              ) : (
+                <i class="fa fa-sign-in fa-lg" aria-hidden="true"></i>
+            )}
           </Link>
           <Link
             to="/wishlist"
             className="avatar avatar-badge text-primary reset"
           >
-          <i className="fas fa-lg fa-heart cart"> </i>
-          { wishlistState.wishCount >0 &&<div className="status-circle icon-top-badge-sm">{wishlistState.wishCount}</div>}
+            <i className="fas fa-lg fa-heart cart"> </i>
+            {wishlistState.wishCount > 0 && (
+              <div className="status-circle icon-top-badge-sm">
+                {wishlistState.wishCount}
+              </div>
+            )}
           </Link>
           <Link to="/cart" className="avatar avatar-badge text-primary reset">
             <i className="fas fa-lg fa-shopping-cart cart"> </i>
-            {cartState.cartCount>0 && <div className="status-circle icon-top-badge-sm">{cartState.cartCount}</div>}
+            {cartState.cartCount > 0 && (
+              <div className="status-circle icon-top-badge-sm">
+                {cartState.cartCount}
+              </div>
+            )}
           </Link>
         </div>
       </header>
@@ -89,11 +101,10 @@ const Navbar = () => {
             className="search_input font2"
             placeholder="SEARCH...."
             value={searchText}
-            onChange={(e)=>{
-              setSearchText(e.target.value.toLowerCase())
-              navigate("/products")
-            }
-            }
+            onChange={(e) => {
+              setSearchText(e.target.value.toLowerCase());
+              navigate("/products");
+            }}
           />
           <i
             className="fa fa-times fa-2x search-close"
